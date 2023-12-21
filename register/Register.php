6ed532766f3b318bcf572269f385576a99d8dc93
HaulaAbdii <muhammadbadar.22085@mhs.unesa.ac.id>
2023-12-21 16:56:29 +0700

    update.css

--- register/Register.php
+++ register/Register.php
@@ -31,6 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Web | Vehicle Rental Service</title> 
+    <link rel="stylesheet" type="text/css" href="../assets/css/login.css">
     <div class="main-header">
         <div class="navbar-header">
             <div class="icon-header">
